58fa6ee2a5bb5fdfe27207b393ed9dfdaf5f60fe
Ehsan <47227409+OracleMatrix@users.noreply.github.com>
2024-04-17 22:48:18 +0330

    Update Main.py
    Added time zone option with sunrise and sunset

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 from customtkinter import *
 import requests
 from tkinter import messagebox
+import time
+from datetime import datetime
 
 root = CTk()
 root.title('Weather APP')
@@ -30,11 +32,20 @@ def get_info():
         pressure = data["main"]["pressure"]
         humidity = data["main"]["humidity"]
         temperature_celsius = int(temperature - 273.15)
+        date = data['dt']
+        sunrise = data['sys']['sunrise']
+        sunset = data['sys']['sunset']
+        date_human = datetime.utcfromtimestamp(date).strftime('%Y-%m-%d %H:%M')
+        sunrise_human = datetime.utcfromtimestamp(sunrise).strftime('%H:%M:%S')
+        sunset_human = datetime.utcfromtimestamp(sunset).strftime('%H:%M:%S')
         temperature_info_label.configure(text=f"Temp {temperature_celsius}")
         humidity_info_label.configure(text=f"Humidity {humidity}")
         pressure_info_label.configure(text=f"Pressure {pressure}")
         air_condition_label.configure(text="Air Condition:")
         air_condition_info_label.configure(text=weather)
+        date_label.configure(text=date_human)
+        sunrise_label.configure(text=f"Sunrise: {sunrise_human}")
+        sunset_label.configure(text=f"Sunset: {sunset_human}")
         if weather == 'Clouds':
             weather_image_label.configure(image=clouds_weather_image)
         elif weather == "Clear":
@@ -79,10 +90,10 @@ pressure_info_label = CTkLabel(root, text="...", font=("Comic Sans MS", 20, "bol
 pressure_info_label.place(relx=0.8, rely=0.75, anchor=CENTER)
 
 air_condition_label = CTkLabel(root, text="", font=("Comic Sans MS", 20, "bold"), bg_color="#7ecbff")
-air_condition_label.place(relx=0.2, rely=0.5, anchor=CENTER)
+air_condition_label.place(relx=0.2, rely=0.35, anchor=CENTER)
 
 air_condition_info_label = CTkLabel(root, text="", font=("Comic Sans MS", 20, "bold"), bg_color="#7ecbff")
-air_condition_info_label.place(relx=0.5, rely=0.6, anchor=CENTER)
+air_condition_info_label.place(relx=0.5, rely=0.65, anchor=CENTER)
 
 clear_weather_image = PhotoImage(file="icons8-weather-96.png")
 clouds_weather_image = PhotoImage(file="icons8-clouds-100.png")
@@ -92,6 +103,15 @@ stormy_weather_image = PhotoImage(file="icons8-stormy-64.png")
 
 weather_image_label = CTkLabel(root, text="", image=None, bg_color="#7ecbff")
 weather_image_label.place(relx=0.5, rely=0.43, anchor="center")
+
+date_label = CTkLabel(root, text='', bg_color="#7ecbff", font=("Comic Sans MS", 20, "bold"))
+date_label.place(relx=0.82, rely=0.45, anchor=CENTER)
+
+sunrise_label = CTkLabel(root, text='', bg_color="#7ecbff", font=("Comic Sans MS", 15, "bold"))
+sunrise_label.place(relx=0.82, rely=0.55, anchor=CENTER)
+
+sunset_label = CTkLabel(root, text='', bg_color="#7ecbff", font=("Comic Sans MS", 15, "bold"))
+sunset_label.place(relx=0.82, rely=0.60, anchor=CENTER)
 
 root.bind("<Return>", lambda event: get_info())
 
